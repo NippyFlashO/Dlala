1ecb85e7dbf3934911bca65a8729fbc91680977e
NippyFlashO <64879872+NippyFlashO@users.noreply.github.com>
2020-05-18 21:28:03 +0000

    Add files via upload

---
+++ RESSOURCES/js/script.js
@@ -0,0 +1,50 @@
+
+
+
+var swiper = new Swiper('.sc1', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.sp1',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+ 
+
+
+
+var swiper = new Swiper('.sc2', {
+    slidesPerView: 6,
+    spaceBetween: 10,
+    freeMode: true,
+    pagination: {
+      el: '.sp2',
+      clickable: true,
+    },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+var swiper = new Swiper('.sc3', {
+    slidesPerView: 6,
+    spaceBetween: 10,
+    freeMode: true,
+    pagination: {
+      el: '.sp2',
+      clickable: true,
+    },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
